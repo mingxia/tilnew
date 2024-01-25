81b6d958a1aa9c465ad124e7390790a013ee9cfd
mingxia <fu.shuaifeng@163.com>
2024-01-25 12:19:36 +0800

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -54,7 +54,7 @@ module.exports = {
         transparent: "transparent",
       },
       fontFamily: {
-        mono: ["IBM Plex Mono", "monospace"],
+        mono: ["LXGW WenKai", "IBM Plex Mono", "monospace"],
       },
 
       typography: {
