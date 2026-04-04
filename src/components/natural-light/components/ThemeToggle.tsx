8671084a70ed7dad5038486e7ca653916266e5f6
mingxia <fu.shuaifeng@163.com>
2026-04-04 09:11:41 +0800

    继续纠错

--- src/components/natural-light/components/ThemeToggle.tsx
+++ src/components/natural-light/components/ThemeToggle.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { useNaturalLight } from '../context/ThemeContext';
 
 /**
--- src/components/natural-light/context/ThemeContext.tsx
+++ src/components/natural-light/context/ThemeContext.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import { createContext, useContext, useEffect, useState, useCallback } from 'react';
 
 // Types
